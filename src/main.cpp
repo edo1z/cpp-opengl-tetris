7ff20bda8b7bae0189ac2895a3a9e416426351b4
web3ten0 <web3ten0@gmail.com>
2022-08-11 01:17:05 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ GLint makeShader()
   const char *fragment_shader = "#version 150\n"
                                 "out vec4 frag_colour;"
                                 "void main() {"
-                                "  frag_colour = vec4(0.0, 0.0, 0.0, 1.0);"
+                                "  frag_colour = vec4(0.1, 0.1, 0.1, 1.0);"
                                 "}";
   GLuint      vs, fs;
   GLuint      shader_programme;
@@ -40,16 +40,28 @@ GLint makeShader()
   return shader_programme;
 }
 
-void updateBgColor(std::vector<GLfloat> &v)
+void updateBgColor(std::vector<GLfloat> &v, bool &isUpping)
 {
-  if (v[0] < 1) {
-    v[0] += 0.1;
-  } else if (v[1] < 1) {
-    v[1] += 0.1;
-  } else if (v[2] < 1) {
-    v[2] += 0.1;
+  if (isUpping) {
+    if (v[0] <= 1) {
+      v[0] += 0.1;
+    } else if (v[1] <= 1) {
+      v[1] += 0.1;
+    } else if (v[2] <= 1) {
+      v[2] += 0.1;
+    } else {
+      isUpping = false;
+    }
   } else {
-    v = { 0 };
+    if (v[0] >= 0) {
+      v[0] -= 0.1;
+    } else if (v[1] >= 0) {
+      v[1] -= 0.1;
+    } else if (v[2] >= 0) {
+      v[2] -= 0.1;
+    } else {
+      isUpping = true;
+    }
   }
 }
 
@@ -58,9 +70,10 @@ int main()
   GLFWwindow          *window = NULL;
   GLuint               vao;
   GLuint               vbo;
-  std::vector<GLfloat> bgColor  = { 0.0 };
+  std::vector<GLfloat> bgColor         = { 0.0f, 0.0f, 0.0f };
+  bool                 isBgColorUpping = true;
 
-  GLfloat              points[] = {
+  GLfloat              points[]        = {
     0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f
   };
 
@@ -102,7 +115,7 @@ int main()
 
   GLint shader = makeShader();
   while (! glfwWindowShouldClose(window)) {
-    updateBgColor(bgColor);
+    updateBgColor(bgColor, isBgColorUpping);
     glClearColor(bgColor[0], bgColor[1], bgColor[2], 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glUseProgram(shader);

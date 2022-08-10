6222b1a909c3e0c049fcaf1c186c77ff5a053a52
web3ten0 <web3ten0@gmail.com>
2022-08-11 00:23:28 +0900

    first

---
+++ Makefile
@@ -0,0 +1,20 @@
+CPPFLAGS := -g -Wall -std=c++17 -I src/includes -I ${CPATH}
+FRAMEWORKS := -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
+LIBS := -L ${LIBRARY_PATH} -lglfw -lGLEW
+
+BUILD_DIR := ./bin
+SRC_DIRS := ./src
+SRCS := $(shell find $(SRC_DIRS) -name *.cpp)
+OBJS := $(addprefix $(BUILD_DIR)/, $(subst .cpp,.o, $(notdir $(SRCS))))
+
+$(BUILD_DIR)/main: $(OBJS)
+	clang++ -o $@ $(LIBS) $(FRAMEWORKS) $^ 
+$(BUILD_DIR)/%.o: src/%.cpp
+	clang++ -c -o $@ $(CPPFLAGS) $<
+
+do:
+	@bin/main
+clean:
+	rm -rf bin
+	mkdir bin
+
---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#ifdef __APPLE__
+#  define GL_SILENCE_DEPRECATION
+#  include <GL/glew.h>
+#  include <GLFW/glfw3.h>
+#endif
+#include <iostream>
+
+static void key_callback(
+    GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+GLint makeShader()
+{
+  const char *vertex_shader   = "#version 150\n"
+                                "in vec3 vp;"
+                                "void main() {"
+                                "  gl_Position = vec4(vp, 1.0);"
+                                "}";
+  const char *fragment_shader = "#version 150\n"
+                                "out vec4 frag_colour;"
+                                "void main() {"
+                                "  frag_colour = vec4(0.3, 0.6, 1.0, 1.0);"
+                                "}";
+  GLuint      vs, fs;
+  GLuint      shader_programme;
+  vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, NULL);
+  glCompileShader(vs);
+  fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, NULL);
+  glCompileShader(fs);
+  shader_programme = glCreateProgram();
+  glAttachShader(shader_programme, fs);
+  glAttachShader(shader_programme, vs);
+  glLinkProgram(shader_programme);
+  return shader_programme;
+}
+
+int main()
+{
+  GLFWwindow *window = NULL;
+  GLuint      vao;
+  GLuint      vbo;
+
+  GLfloat     points[] = {
+    0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f
+  };
+
+  if (! glfwInit()) return 1;
+  atexit(glfwTerminate);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (! window) {
+    std::cerr << "Failed to create window." << std::endl;
+    return 1;
+  }
+
+  glfwSetKeyCallback(window, key_callback);
+  glfwMakeContextCurrent(window);
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Can't initialize GLEW" << std::endl;
+    return 1;
+  }
+
+  glClearColor(0.0f, 0.1f, 0.5f, 0.0f);
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), points, GL_STATIC_DRAW);
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  GLint shader = makeShader();
+  while (! glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(shader);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+}

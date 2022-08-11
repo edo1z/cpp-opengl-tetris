62b722bea9047fefdd00498ede3be65cfb3e0e6f
web3ten0 <web3ten0@gmail.com>
2022-08-11 18:59:54 +0900

    shaderのソースコードを別ファイル化した

--- src/shader.cpp
+++ src/shader.cpp
@@ -3,21 +3,49 @@
 #  include <GL/glew.h>
 #  include <GLFW/glfw3.h>
 #endif
+#include <fstream>
 #include <iostream>
 #include <vector>
 
+bool readShaderSource(const char* name, std::vector<GLchar>& buffer)
+{
+  if (name == NULL) return false;
+  std::ifstream file(name);
+  if (file.fail()) {
+    std::cerr << "Error: Can't open source file: " << name << std::endl;
+    return false;
+  }
+  file.seekg(0L, std::ios::end);
+  GLsizei length = static_cast<GLsizei>(file.tellg());
+  buffer.resize(length + 1);
+  file.seekg(0L, std::ios::beg);
+  file.read(buffer.data(), length);
+  buffer[length] = '\0';
+
+  if (file.fail()) {
+    std::cerr << "Error: Could not read source file: " << name << std::endl;
+    file.close();
+    return false;
+  }
+
+  file.close();
+  return true;
+}
+
 GLint makeShader()
 {
-  const char *vertex_shader   = "#version 150\n"
-                                "in vec3 vp;"
-                                "void main() {"
-                                "  gl_Position = vec4(vp, 1.0);"
-                                "}";
-  const char *fragment_shader = "#version 150\n"
-                                "out vec4 frag_colour;"
-                                "void main() {"
-                                "  frag_colour = vec4(0.1, 0.1, 0.1, 1.0);"
-                                "}";
+  const char*         vertFileName = "src/shaders/point.vert";
+  const char*         flagFileName = "src/shaders/point.frag";
+  std::vector<GLchar> vsrc;
+  std::vector<GLchar> fsrc;
+  const bool          vstat = readShaderSource(vertFileName, vsrc);
+  const bool          fstat = readShaderSource(flagFileName, fsrc);
+  if (! vstat || ! fstat) {
+    std::cerr << "Error: Could not read source file" << std::endl;
+    return 0;
+  }
+  const char* vertex_shader   = vsrc.data();
+  const char* fragment_shader = fsrc.data();
   GLuint      vs, fs;
   GLuint      shader_programme;
   vs = glCreateShader(GL_VERTEX_SHADER);
---
+++ src/shaders/point.frag
@@ -0,0 +1,5 @@
+#version 150
+out vec4 frag_colour;
+void main() {
+  frag_colour = vec4(0.1, 0.1, 0.1, 1.0);
+}
